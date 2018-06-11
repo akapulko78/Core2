09527f75112830bdd649f10c4bab0746db73d176
akapulko78 <dgavrilodv@gmail.com>
2018-06-11 13:15:13 +0300

    Lesson4

---
+++ src/main/java/com/gavrilov/lesson4/Starter.java
@@ -0,0 +1,9 @@
+package com.gavrilov.lesson4;
+
+import javax.swing.*;
+
+public class Starter {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new ChatWindow());
+    }
+}
